bdfc952830ad31bc87201d456e3c4d7f72ab3fcd
Ubuntu <lincoln.harris@czbiohub.org>
2019-01-11 21:37:20 +0000

    some changes

--- build_samples.py
+++ build_samples.py
@@ -1,4 +1,4 @@
-#////////////////////////////////////////////////////////////////////
+ #////////////////////////////////////////////////////////////////////
 #////////////////////////////////////////////////////////////////////
 # script: build_samples.py
 # authors: Lincoln Harris
@@ -54,19 +54,43 @@ def get_fastqs_R2(path):
 
 cwd = os.getcwd()
 
-f = 'cellNames.txt'
-get_ipython().system('ls {cwd}/170202/ > $f')
 
-# read cell names into a pandas df
-cells_df = pd.read_table(f, delim_whitespace=True, header=None, names=['cell_name'])
+bucketPrefixes = get_ipython().system('aws s3 ls s3://darmanis-group/singlecell_lungadeno/non_immune/nonImmune_fastqs_9.27/')
+f = 'bucketPrefixes.txt'
+get_ipython().system('aws s3 ls $bucketPrefixes > $f')
+
+# read run prefixes into a pandas df
+runs_df = pd.read_table(f, delim_whitespace=True, header=None, names=['is_prefix', 'run_name'])
 
 # add a full_path col
-cells_df['full_path'] = cwd + '/' + '170202/' + cells_df['cell_name']
+runs_df['full_path'] = 's3://darmanis-group/singlecell_lungadeno/non_immune/nonImmune_fastqs_9.27/' + runs_df['run_name']
 
-# add input_fq1/2 cols
-cells_df['input_fq1'] = cells_df['full_path'].map(get_fastqs_R1) 
-cells_df['input_fq2'] = cells_df['full_path'].map(get_fastqs_R2) 
-print(cells_df)
+for i in range(0, len(runs_df.index)):
+	global prefix
+	prefix = runs_df['full_path'][i]
+	print(prefix)
+
+	get_ipython().system('aws s3 cp $prefix . --recursive')
+
+	# read cell names into a pandas df
+	cells_df = pd.read_table(txt, delim_whitespace=True, header=None, names=['cell_name'])
+
+
+	
+
+
+	# add a full_path col
+	cells_df['full_path'] = cwd + '/' + '170202/' + cells_df['cell_name']
+
+	# add input_fq1/2 cols
+	cells_df['input_fq1'] = cells_df['full_path'].map(get_fastqs_R1) 
+	cells_df['input_fq2'] = cells_df['full_path'].map(get_fastqs_R2) 
+
+	samples_df = cells_df[['cell_name', 'input_fq1', 'input_fq2']]
+	samples_df.to_csv('samples.csv', index=False, sep='\t', header=False)
+
+	# clean up 
+	get_ipython().system('rm *.fastq*')
 
 #////////////////////////////////////////////////////////////////////
 #////////////////////////////////////////////////////////////////////
